2b8db2fccb33884458ce5890f108db58726401ab
PhillipHenry <PhillHenry@gmail.com>
2024-01-22 16:54:05 +0000

    OOMEs

--- src/main/scala/uk/co/odinconsultants/spark/katas/HighLowCardinalityMain.scala
+++ src/main/scala/uk/co/odinconsultants/spark/katas/HighLowCardinalityMain.scala
@@ -8,11 +8,16 @@ object HighLowCardinalityMain {
     import spark.implicits._
     val numLeft: Long  = 10000000L
     val numRight: Long = 1000L
-    val left           = spark.range(0, numLeft).as[Long]
-    def right          = left.mapPartitions { rows: Iterator[Long] =>
-      rows.flatMap(j => Iterator.range(0, numRight).map(i => (i, j)))
-    }
-    println(s"Number of partitions: left = ${numOfPartitionsIn(left)}, right = ${numOfPartitionsIn(right)}")
+    val nPartitions    = 256
+    val left           = spark.range(0, numLeft).as[Long].repartition(nPartitions)
+    def right          = left
+      .mapPartitions { rows: Iterator[Long] =>
+        rows.flatMap(j => Iterator.range(0, numRight).map(i => (i, j)))
+      }
+      .repartition(nPartitions)
+    println(
+      s"Number of partitions: left = ${numOfPartitionsIn(left)}, right = ${numOfPartitionsIn(right)}"
+    )
     val result         = left.join(right, $"id" === $"_2")
     val count          = result.count()
     println(s"Number of partitions: result = ${numOfPartitionsIn(result)}")
