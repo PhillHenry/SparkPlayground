e8ff2bb26ffb9f83203c40ab58b6167063a02088
PhillipHenry <PhillHenry@gmail.com>
2024-01-22 14:16:34 +0000

    Docs.

--- src/main/scala/uk/co/odinconsultants/spark/katas/BigDataSmallMemoryMain.scala
+++ src/main/scala/uk/co/odinconsultants/spark/katas/BigDataSmallMemoryMain.scala
@@ -5,6 +5,13 @@ object BigDataSmallMemoryMain {
 
   /**
    * Run this with -Xmx512m to demonstrate that 1.4gb is indeed written to disk.
+   * On my machine:
+   *
+   * $ tree /tmp/results_parquet
+  /tmp/results_parquet
+  ├── part-00000-edbc76a6-6c03-4f79-9a6d-b650219d261e-c000.snappy.parquet
+  ├── part-00001-edbc76a6-6c03-4f79-9a6d-b650219d261e-c000.snappy.parquet
+  └── _SUCCESS
    */
   def main(args: Array[String]): Unit = {
     import spark.implicits._
@@ -19,6 +26,9 @@ object BigDataSmallMemoryMain {
     val count         = actual.count()
     val expectedRows  = multiplier * initialNumRows
     println(s"Actual numer of rows = $count, expected = $expectedRows")
+    // on my machine:
+    // Number of partitions: Original = 2, persisted = 11
+    println(s"Number of partitions: Original = ${df.rdd.partitions.length}, persisted = ${actual.rdd.partitions.length}")
     assert(count == expectedRows)
   }
 
