ad40e0eccfc1517969b4732f0898a41700823dcc
PhillipHenry <PhillHenry@gmail.com>
2024-01-23 09:37:18 +0000

    This works with 512mb pretty quickly

--- src/main/scala/uk/co/odinconsultants/spark/katas/HighLowCardinalityMain.scala
+++ src/main/scala/uk/co/odinconsultants/spark/katas/HighLowCardinalityMain.scala
@@ -1,20 +1,22 @@
 package uk.co.odinconsultants.spark.katas
+import org.apache.spark.sql.Dataset
 import uk.co.odinconsultants.spark.katas.SparkForTesting.spark
 import uk.co.odinconsultants.spark.katas.SparkUtils.numOfPartitionsIn
 
 object HighLowCardinalityMain {
 
+  import spark.implicits._
+
   def main(args: Array[String]): Unit = {
-    import spark.implicits._
-    val numLeft: Long  = 10000000L
+    val numLeft: Long  = 100000L
     val numRight: Long = 1000L
     val nPartitions    = 256
-    val left           = spark.range(0, numLeft).as[Long].repartition(nPartitions)
-    def right          = left
-      .mapPartitions { rows: Iterator[Long] =>
-        rows.flatMap(j => Iterator.range(0, numRight).map(i => (i, j)))
-      }
-      .repartition(nPartitions)
+    println(s"Creating data frame with $numLeft rows")
+    val left           = spark.range(0, numLeft).as[Long] // .repartition(nPartitions)
+    val fileName       = "/tmp/HighLowCardinalityMain_parquet"
+    if (!new java.io.File(fileName).exists()) highCardinalityToDisk(left, numRight.toInt, fileName)
+    println(s"Reading $fileName")
+    def right          = spark.read.parquet(fileName)     // .repartition(nPartitions)
     println(
       s"Number of partitions: left = ${numOfPartitionsIn(left)}, right = ${numOfPartitionsIn(right)}"
     )
@@ -24,4 +26,17 @@ object HighLowCardinalityMain {
     println(s"count = $count")
   }
 
+  private def highCardinalityToDisk(
+      left:     Dataset[Long],
+      numRight: Int,
+      fileName: String,
+  ): Unit = {
+    println(s"Writing $fileName")
+    left
+      .mapPartitions { rows: Iterator[Long] =>
+        rows.flatMap(j => Iterator.range(0, numRight).map(i => (i, j)))
+      }
+      .write
+      .parquet(fileName)
+  }
 }
